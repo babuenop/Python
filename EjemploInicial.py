72738d1328488cfafd0452d5faf79b62713df95b
babuenop <abuenop@hotmail.com>
2018-05-26 12:01:22 -0500

    turtle — Turtle graphics
    Practica

---
+++ EjemploInicial.py
@@ -0,0 +1,16 @@
+"""
+File: ejemplo_inicial.py
+
+"""
+import turtle
+window = turtle.Screen()
+t = turtle.Turtle()
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+turtle.mainloop()
